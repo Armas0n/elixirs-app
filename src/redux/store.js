6a2c726f60529980d094aa1d8b2e3fc93e947ca6
Armason <necroorc@gmail.com>
2022-01-16 02:55:30 +0300

    fixed redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,8 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer,
-                          compose(
-                              applyMiddleware(thunk),
-                              window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                          ));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
